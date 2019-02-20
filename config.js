cee28a29d56a254ac1637ac005d7936f9117b7b0
SongoMen <pat.kozlowski2000@onet.pl>
2019-02-20 01:52:55 +0100

    config

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBsrU8XddCRVe_sf0elABEClDvQFYu0pqM",
+    authDomain: "bored-23418.firebaseapp.com",
+    databaseURL: "https://bored-23418.firebaseio.com",
+    projectId: "bored-23418",
+    storageBucket: "bored-23418.appspot.com",
+    messagingSenderId: "118012595019"
+  };
+  firebase.initializeApp(config);
